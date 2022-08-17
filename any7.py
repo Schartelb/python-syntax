9f6541b5e80ee0bb50a0079356464e9078ad1395
Schartelb <106553010+Schartelb@users.noreply.github.com>
2022-08-17 10:09:38 -0400

    first commit

---
+++ any7.py
@@ -0,0 +1,14 @@
+import numbers
+
+
+def any7(nums):
+    """Are any of these numbers a 7? (True/False)"""
+
+
+for num in nums:
+    if num == 7:
+        print("A number is 7")
+
+
+print("should be true", any7([1, 2, 7, 4, 5]))
+print("should be false", any7([1, 2, 4, 5]))
